77e303947b8bd3dd6240154568c8e4d7f399f8a0
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-08-26 21:43:37 +0330

    add event publishing to proc

--- proc.go
+++ proc.go
@@ -2,8 +2,254 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"sync"
+	"sync/atomic"
+	"syscall"
+
+	"github.com/cskr/pubsub/v2"
 )
 
-type Proc interface {
-	Run(ctx context.Context) error
+type ProcState int
+
+const (
+	ProcStateNotStarted ProcState = iota
+	ProcStateStarting
+	ProcStateStarted
+	ProcStateStopped
+	ProcStateDone
+)
+
+type ProcTopic int
+
+const procTopic ProcTopic = 0
+
+var (
+	ProcErrNotStarted = errors.New("not started yet")
+	ProcErrNotStopped = errors.New("not stopped yet")
+)
+
+type Proc struct {
+	Name string
+	Args []string
+
+	Stdin io.Reader
+
+	Cwd string
+	Uid uint32
+	Gid uint32
+	Env []string
+
+	state atomic.Int32
+
+	stdout *Broadcaster
+	stderr *Broadcaster
+
+	cmd      *exec.Cmd
+	exitCode atomic.Int32
+
+	bus *pubsub.PubSub[ProcTopic, ProcState]
+}
+
+func (this *Proc) Start(ctx context.Context) error {
+	err := this.setState(ProcStateStarting)
+	if err != nil {
+		return err
+	}
+
+	this.setCmd(ctx)
+	stdout, err := this.cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := this.cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	err = this.cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	err = this.setState(ProcStateStarted)
+	if err != nil {
+		return err
+	}
+
+	err = this.waitForCmd(stdout, stderr)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (this *Proc) StdoutPipe() io.ReadCloser {
+	return this.pipe(this.stdout)
+}
+
+func (this *Proc) StderrPipe() io.ReadCloser {
+	return this.pipe(this.stderr)
+}
+
+func (this *Proc) GetPid() (int, error) {
+	if this.GetState() < ProcStateStarted {
+		return 0, ProcErrNotStarted
+	}
+
+	return this.cmd.Process.Pid, nil
+}
+
+func (this *Proc) Signal(signal os.Signal) error {
+	if this.GetState() < ProcStateStarted {
+		return ProcErrNotStarted
+	}
+
+	return this.cmd.Process.Signal(signal)
+}
+
+func (this *Proc) GetExitCode() (int, error) {
+	if this.GetState() < ProcStateStopped {
+		return 0, ProcErrNotStopped
+	}
+
+	return int(this.exitCode.Load()), nil
+}
+
+func (this *Proc) GetState() ProcState {
+	return ProcState(this.state.Load())
+}
+
+// Subscribe to process's state changes
+func (this *Proc) Sub() chan ProcState {
+	return this.bus.Sub(procTopic)
+}
+
+// Unsubscribe from process's state changes
+func (this *Proc) Unsub(ch chan ProcState) {
+	this.bus.Unsub(ch)
+}
+
+// Close all subscribed channels and release resources.
+func (this *Proc) Shutdown() {
+	this.bus.Shutdown()
+}
+
+func (this *Proc) pipe(b *Broadcaster) io.ReadCloser {
+	r, w := io.Pipe()
+	b.Add(w)
+	return r
+}
+
+func (this *Proc) setCmd(ctx context.Context) {
+	cmd := exec.CommandContext(ctx, this.Name, this.Args...)
+
+	if this.Cwd != "" {
+		cmd.Dir = this.Cwd
+	}
+	if this.Env != nil {
+		cmd.Env = this.Env
+	}
+	if this.Stdin != nil {
+		cmd.Stdin = this.Stdin
+	}
+
+	if this.Uid != uint32(syscall.Getuid()) ||
+		this.Gid != uint32(syscall.Getgid()) {
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Credential: &syscall.Credential{
+				Uid: this.Uid,
+				Gid: this.Gid,
+			},
+		}
+	}
+
+	this.cmd = cmd
+}
+
+func (this *Proc) waitForCmd(stdout, stderr io.ReadCloser) error {
+	var wg sync.WaitGroup
+	// 1: stdout broadcaster
+	// 2: stderr broadcaster
+	// 3: command execution
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+
+		this.flush(this.stdout, stdout)
+	}()
+	go func() {
+		defer wg.Done()
+
+		this.flush(this.stderr, stderr)
+	}()
+	go func() {
+		defer wg.Done()
+
+		err := this.cmd.Wait()
+		if err != nil {
+			fmt.Println("process:", err)
+
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				this.exitCode.Store(int32(exitErr.ExitCode()))
+			}
+		}
+
+		this.setState(ProcStateStopped)
+	}()
+
+	wg.Wait()
+	return this.setState(ProcStateDone)
+}
+
+func (this *Proc) setState(state ProcState) error {
+	curr := this.state.Load()
+	if curr >= int32(state) {
+		return fmt.Errorf(
+			"process state can only go forward: current: %d, desired: %d",
+			curr, state,
+		)
+	}
+
+	this.state.Store(int32(state))
+	this.bus.Pub(state, procTopic)
+
+	return nil
+}
+
+func (this *Proc) flush(b *Broadcaster, out io.ReadCloser) {
+	err := b.Run(out)
+	if err == os.ErrClosed {
+		return
+	}
+
+	if err != nil {
+		fmt.Println("process: broadcaster:", err)
+	}
+
+	out.Close()
+}
+
+func NewProc(name string, args ...string) *Proc {
+	return &Proc{
+		Name:  name,
+		Args:  args,
+		Stdin: nil,
+		Cwd:   "",
+		Uid:   uint32(syscall.Getuid()),
+		Gid:   uint32(syscall.Getgid()),
+		Env:   nil,
+
+		state:  atomic.Int32{},
+		stdout: NewBroadcaster(),
+		stderr: NewBroadcaster(),
+
+		bus: pubsub.New[ProcTopic, ProcState](0),
+	}
 }
---
+++ proc_test.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"context"
+	"io"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+)
+
+type ProcPipesTest struct {
+	t      *testing.T
+	shell  string
+	script string
+	stdout string
+	stderr string
+}
+
+func (this *ProcPipesTest) Run() {
+	p := NewProc(this.shell)
+	p.Stdin = strings.NewReader(this.script)
+	stdout := p.StdoutPipe()
+	defer stdout.Close()
+	stderr := p.StderrPipe()
+	defer stderr.Close()
+
+	ctx, cancel := context.WithTimeout(this.t.Context(), time.Second)
+	defer cancel()
+
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+
+		err := p.Start(ctx)
+		if err != nil {
+			this.t.Error(err)
+			this.t.Fail()
+		}
+	}()
+
+	this.assertOutput(stdout, this.stdout)
+	this.assertOutput(stderr, this.stderr)
+
+	<-done
+}
+
+func (this *ProcPipesTest) assertOutput(
+	out io.Reader, expected string,
+) {
+	b, err := io.ReadAll(out)
+	if err != nil {
+		this.t.Error(err)
+		this.t.Fail()
+	}
+	if string(b) != expected {
+		this.t.Errorf("unexpected standard output: %s", string(b))
+		this.t.Fail()
+	}
+}
+
+func TestProcPipes(t *testing.T) {
+	pt := ProcPipesTest{
+		t,
+		"/usr/bin/sh",
+		`echo -n stdout
+		echo -n stderr >&2`,
+		"stdout",
+		"stderr",
+	}
+	pt.Run()
+}
+
+type ProcStatesTest struct {
+	t       *testing.T
+	shell   string
+	script  string
+	timeout time.Duration
+}
+
+func (this *ProcStatesTest) Run() {
+	p := NewProc(this.shell)
+	p.Stdin = strings.NewReader(this.script)
+
+	ctx, cancel := context.WithTimeout(this.t.Context(), this.timeout)
+	defer cancel()
+
+	states := p.Sub()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+
+		this.assertEvents(states)
+	}()
+	go func() {
+		defer wg.Done()
+
+		err := p.Start(ctx)
+		if err != nil {
+			this.t.Error(err)
+			this.t.Fail()
+		}
+
+		p.Unsub(states)
+		p.Shutdown()
+	}()
+
+	wg.Wait()
+}
+
+func (this *ProcStatesTest) assertEvents(states chan ProcState) {
+	shouldBe := func(expected, received ProcState) {
+		if received != expected {
+			this.t.Errorf(
+				"unexpected state: expected: %d received: %d",
+				expected, received,
+			)
+			this.t.Fail()
+		}
+	}
+
+	i := 0
+
+	for state := range states {
+		switch i {
+		case 0:
+			shouldBe(ProcStateStarting, state)
+		case 1:
+			shouldBe(ProcStateStarted, state)
+		case 2:
+			shouldBe(ProcStateStopped, state)
+		case 3:
+			shouldBe(ProcStateDone, state)
+		}
+
+		i++
+	}
+
+	if i != 4 {
+		this.t.Errorf("expected 4 state changes received %d", i)
+		this.t.Fail()
+	}
+}
+
+func TestProcStates(t *testing.T) {
+	pt1 := ProcStatesTest{
+		t,
+		"/usr/bin/sh",
+		"sleep 1",
+		time.Second * 5,
+	}
+	pt1.Run()
+
+	var wg sync.WaitGroup
+	n := 100
+	wg.Add(n)
+
+	for i := 0; i < n; i++ {
+		pt := ProcStatesTest{
+			t,
+			"/usr/bin/sh",
+			"sleep 1",
+			time.Second * 10,
+		}
+
+		go func() {
+			defer wg.Done()
+
+			pt.Run()
+		}()
+	}
+
+	wg.Wait()
+}
